e4c0c1e87fe6fb07a878a0367287f9f5cdd02441
55dreev <aada2022-5733-94833@bicol-u.edu.ph>
2024-10-03 21:33:15 +0800

    Update user.blade.php

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -9,8 +9,6 @@
 @section('content')
     <h1>Welcome, {{ $username }}</h1>
     <p>Enjoy exploring the gaming hub!</p>
-    <p>Username: {{ $username }}</p>
-        <p>Age: {{ $age }}</p>
         <p>Status: {{ $verificationStatus }}</p> <!-- Display the verification status -->
     <button class="button" onclick="location.href='{{ url('/gallery') }}'">Explore Gallery</button>
 @endsection
